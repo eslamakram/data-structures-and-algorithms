a7f688d263f06d7d70dab1bb6228eea346bc3927
Eslam Akram <eslam_akram91@yahoo.com>
2021-11-06 03:54:44 +0200

    fixed ch02

--- python/code_challenges/array-insert-shift/array-insert-shift.py
+++ python/code_challenges/array-insert-shift/array-insert-shift.py
@@ -1,19 +1,26 @@
 
 def array_insert_shift(array,addedValue):
 
-    end = len(array)
-    middleIndex = len(array)//2 +1
-    array =array + [0]
-    while end >= middleIndex:
-       array[end]= array[end-1]
-       end -=1
+    # end = len(array)
+    middleIndex = len(array)//2
+    # array =array + [0]
+    # while end >= middleIndex:
+    #    array[end]= array[end-1]
+    #    end -=1
 
-       array[-middleIndex]= addedValue
+    #    array[-middleIndex]= addedValue
 
-    return array
+    result = [0]*(len(array)+1)
+    for i in range( middleIndex):
+        result[i] = array[i]
+    result[middleIndex] = addedValue
+    for j in range(middleIndex +1 , len(result)):
+        result[j] = array[j -1]
+
+    return result
 
 
-  
+
 if __name__ == "__main__":
 
   array = [2,4,6,-8]
