93c6ae517301c4dad65020927ec242ff88399dda
Eslam Akram <eslam_akram91@yahoo.com>
2021-11-07 23:44:33 +0200

    finished

---
+++ python/code_challenges/linked-list-insertions/linked-list-insertions/linked_list_insertions/linked_list_insertions.py
@@ -0,0 +1,100 @@
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+    def append (self,value):
+        node = Node(value)
+
+        if self.head is None:
+            self.head = node
+        else:
+            currentNode = self.head
+            while currentNode.next != None:
+                  currentNode = currentNode.next
+            currentNode.next = node
+
+
+
+    def Insert_After_Item(self,item,data):
+        node = self.head
+        while node is not None:
+            if node.value == item:
+                break
+            node = node.next
+        if node is None:
+            print("item not in the list")
+        else:
+            new_node = Node(data)
+            new_node.next = node.next
+            node.next = new_node
+
+
+    def Insert_Before_Item(self, item, data):
+        if self.head is None:
+            print("List has no element")
+            return
+
+        if item == self.head.value:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        current_node = self.head
+        print(current_node.next)
+        while current_node.next is not None:
+            if current_node.next.value == item:
+                break
+            current_node = current_node.next
+        if current_node.next is None:
+            print("item not in the list")
+        else:
+            new_node = Node(data)
+            new_node.next = current_node.next
+            current_node.next = new_node
+
+
+
+
+    def __str__(self):
+        # "head -> 1 -> 2 -> 3 -> 4 -> None"
+        output = "Head -> "
+        if self.head is None:
+            output += None
+
+        else:
+            current = self.head
+            while current:
+                output += f"{current.value} -> "
+                current = current.next
+
+            output += "None"
+            return output
+
+
+
+
+if __name__ == "__main__":
+     Linked_List_test = Linked_List()
+
+     Linked_List_test.append(1)
+     Linked_List_test.append(1)
+     Linked_List_test.append(1991)
+     Linked_List_test.append("welcome")
+
+     print (Linked_List_test.Insert_Before_Item(1991,4))
+     print (Linked_List_test.Insert_After_Item(1991,"Ansam"))
+
+
+     print(Linked_List_test)
+
+
+
+
---
+++ python/code_challenges/linked-list-insertions/linked-list-insertions/tests/test_linked_list_insertions.py
@@ -0,0 +1,64 @@
+from linked_list_insertions import __version__
+
+from linked_list_insertions.linked_list_insertions import Node, Linked_List
+
+import pytest
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+# Can successfully add a node to the end of the linked list
+# Can successfully add multiple nodes to the end of a linked list
+# Can successfully insert a node before a node located i the middle of a linked list
+# Can successfully insert a node before the first node of a linked list
+# Can successfully insert after a node in the middle of the linked list
+# Can successfully insert a node after the last node of the linked list
+
+
+def test_add_node_linked_list():
+    ll = Linked_List()
+    expected = None
+    actual = ll.head
+    assert expected == actual
+
+def test_add_multiple_nodes_to_existing_ll(ll):
+    ll.append(True)
+    ll.append("Hello")
+    expected = 'Head -> 1 -> Hello -> 1991 -> 6 -> 7 -> True -> Hello -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+def test_insert_node_before_middle_ll(ll):
+    ll.Insert_Before_Item(1991, "Mid")
+    expected = 'Head -> 1 -> Hello -> Mid -> 1991 -> 6 -> 7 -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+
+def test_insert_node_before_first_ll(ll):
+    ll.Insert_Before_Item(1, "First")
+    expected = 'Head -> First -> 1 -> Hello -> 1991 -> 6 -> 7 -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+def test_insert_node_after_middle_ll(ll):
+    ll.Insert_After_Item(1991, "Mid")
+    expected = 'Head -> 1 -> Hello -> 1991 -> Mid -> 6 -> 7 -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+def test_insert_node_after_first_ll(ll):
+     ll.Insert_After_Item(1,"First")
+     expected = 'Head -> 1 -> First -> Hello -> 1991 -> 6 -> 7 -> None'
+     actual = ll.__str__()
+     assert expected == actual
+
+@pytest.fixture
+def ll():
+    ll = Linked_List()
+    ll.append(1)
+    ll.append('Hello')
+    ll.append(1991)
+    ll.append(6)
+    ll.append(7)
+    return ll
