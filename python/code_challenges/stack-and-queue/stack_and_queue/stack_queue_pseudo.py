0043180fcdfd28310cd9bb53446e89a1fccaf5ad
Eslam Akram <eslam_akram91@yahoo.com>
2021-11-14 23:51:51 +0200

    finished

---
+++ python/code_challenges/stack-and-queue/stack_and_queue/stack_queue_pseudo.py
@@ -0,0 +1,25 @@
+from stack_and_queue import  Stack
+
+class PseudoQueue:
+      def __init__(self):
+          self.Push_Stack = Stack()
+          self.Pop_Stack = Stack()
+
+      def enqueue(self,value):
+        self.Push_Stack.push(value)
+
+      def dequeue(self):
+         if self.Pop_Stack.is_empty():
+            while self.Push_Stack.top != None:
+                  self.Pop_Stack.push(self.Push_Stack.pop())
+         return (self.Pop_Stack.pop())
+
+if __name__ == "__main__":
+    testQueue=PseudoQueue()
+    testQueue.enqueue(3)
+    testQueue.enqueue(4)
+    testQueue.enqueue(6)
+    testQueue.enqueue(7)
+    print(testQueue.dequeue())
+    print(testQueue.dequeue())
+    print(testQueue.dequeue())
