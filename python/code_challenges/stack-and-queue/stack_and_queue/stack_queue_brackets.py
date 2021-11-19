6ce2ed2805aed23df6dded3c89d7e50f87029b34
Eslam Akram <eslam_akram91@yahoo.com>
2021-11-19 23:28:44 +0200

    fixed ch 13

---
+++ python/code_challenges/stack-and-queue/stack_and_queue/stack_queue_brackets.py
@@ -0,0 +1,39 @@
+from stack_and_queue import Stack
+def Multi_Bracket_Validation(str):
+
+    open_brackect_list = ["(","[","{"]
+    close_brackect_list= [")","]","}"]
+    stack = Stack()
+    for bracket in str:
+        if bracket in open_brackect_list:
+            stack.push(bracket)
+        elif bracket in close_brackect_list:
+            if not stack.is_empty():
+                index_bracket = stack.pop()
+                if (open_brackect_list.index(index_bracket) != close_brackect_list.index(bracket)):
+                    return False
+                else:
+                    continue
+            else:
+                return False
+    if stack.is_empty() :
+        return True
+    else:
+        return False
+
+# {}	TRUE
+# {}(){}	TRUE
+# ()[[Extra Characters]]	TRUE
+# (){}[[]]	TRUE
+# {}{Code}[Fellows](())	TRUE
+# [({}]	FALSE
+# (](	FALSE
+# {(})	FALSE
+
+if __name__ == "__main__":
+
+    print(Multi_Bracket_Validation('()}})')) # false
+    print(Multi_Bracket_Validation('{}{Code}[Fellows](())'))  #true
+    print(Multi_Bracket_Validation('{)'))
+    print(Multi_Bracket_Validation('{}(){}'))
+    print(Multi_Bracket_Validation('{}'))
---
+++ python/code_challenges/stack-and-queue/tests/test_stack_queue_brackets.py
@@ -0,0 +1,29 @@
+
+from stack_and_queue.stack_queue_brackets import Multi_Bracket_Validation
+
+def test_multi_brackets_1():
+    expected = True
+    actual = Multi_Bracket_Validation('{}{Code}[Fellows](())')
+    assert expected == actual
+
+def test_multi_brackets_2():
+    expected = False
+    actual = Multi_Bracket_Validation('[({}]')
+    assert expected == actual
+
+def test_multi_brackets_3():
+    expected = False
+    actual = Multi_Bracket_Validation('{(})')
+    assert expected == actual
+
+
+def test_multi_brackets_4():
+    expected = True
+    actual = Multi_Bracket_Validation('(){}[[]]')
+    assert expected == actual
+
+
+def test_multi_brackets_5():
+    expected = True
+    actual = Multi_Bracket_Validation('()[[Extra Characters]]')
+    assert expected == actual
